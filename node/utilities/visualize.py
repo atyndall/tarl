e283794822772a151140957e15f0b00de146b1de
Ash Tyndall <git@atyndall.net>
2015-01-14 15:33:30 +0800

    Update visualise to work on Windows with the multiprocessing library

--- node/utilities/visualize.py
+++ node/utilities/visualize.py
@@ -1,5 +1,8 @@
 import tcam
+from multiprocessing import Process, freeze_support
 
-tc = tcam.TempCam("COM4")
-tcv = tcam.Video(tc)
-tcv.display(block=True, tmin=15, tmax=40)
+if __name__ == '__main__':
+  freeze_support()
+  tc = tcam.TempCam("COM4")
+  tcv = tcam.Video(tc)
+  tcv.display(block=True, tmin=15, tmax=35)
