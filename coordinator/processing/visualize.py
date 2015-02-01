5cf549d3b69ec76b19d4e78b9bca4d7c60cf65d3
Ash Tyndall <git@atyndall.net>
2015-02-01 11:51:59 +0800

    Make visualizer cross-platform

--- coordinator/processing/visualize.py
+++ coordinator/processing/visualize.py
@@ -1,8 +1,9 @@
 from thinglib import *
 from multiprocessing import Process, freeze_support
+import sys
 
 if __name__ == '__main__':
   freeze_support()
-  tc = cam.Manager("COM4")
+  tc = cam.Manager(sys.argv[1])
   tcv = cam.Visualizer(tc)
-  tcv.display(block=True, tmin=15, tmax=35)
+  tcv.display(block=True, tmin=15, tmax=50, width=80)
