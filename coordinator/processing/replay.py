2e186c92b1b1e9ac985ea5bffd3d96d39f54f2ca
Ash Tyndall <git@atyndall.net>
2015-02-01 11:54:32 +0800

    Basic thermal capture replay utility

---
+++ coordinator/processing/replay.py
@@ -0,0 +1,22 @@
+from thinglib import *
+import time
+import sys
+from multiprocessing import Process, freeze_support
+
+if __name__ == '__main__':
+  freeze_support()
+  tc = cam.ManagerPlaybackEmulator()
+  tcv = cam.Visualizer(tc)
+
+  cap = tcv.file_to_capture(sys.argv[1])
+  tc.set_playback_data(cap)
+
+  feat = features.Features(tc.subscribe(), int(tc.irhz), motion_window=2)
+
+  tcv.display()
+  tc.start()
+
+  time.sleep(9999)
+
+  tc.close()
+  tcv.close()
